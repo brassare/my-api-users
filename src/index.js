1e60377c7f6d6b0c83fe59284ead6cdd441a0c3f
brassare <brassare@hotmail.com>
2025-10-14 19:27:55 -0300

    Criando um metodo response.send , para não ficar repetindo codigo de retorno

--- src/index.js
+++ src/index.js
@@ -25,9 +25,15 @@ const server = http.createServer((request, response) => {
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams)
     request.params = { id }
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' }),
+        response.end(JSON.stringify(body));
+    };
+
     route.handler(request, response)
   } else {
-    response.writeHead(404, { 'content-type': 'text/html' })
+    response.writeHead(404, { 'Content-Type': 'text/html' })
     response.end(`Cannot $${request.method} ${parsedUrl.pathname}`)
   }
 
